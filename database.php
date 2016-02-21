b3be77b0c5058fcde1b3dc7ac5e905541bfd2f02
raymonst <rsutedjothe@rsutedjothe-ltm.internal.salesforce.com>
2016-02-21 01:09:45 -0800

    mysql change

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-  $servername = "eatux.ccistyooeowb.us-west-2.rds.amazonaws.com:22";
+  $servername = "eatux.ccistyooeowb.us-west-2.rds.amazonaws.com:3306";
   $username = "eatux";
   $password = "Lunch123!";
   $database = "eatux";
