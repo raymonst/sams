a72919504b9973bc794fa738e6322e71aeaa6adf
raymonst <rsutedjothe@rsutedjothe-ltm.internal.salesforce.com>
2016-02-21 01:37:36 -0800

    heroku mysql update

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 
-  $servername = "eatux.ccistyooeowb.us-west-2.rds.amazonaws.com:3306";
-  $username = "eatux";
-  $password = "Lunch123!";
-  $database = "eatux";
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "be0f13c51d03a5";
+  $password = "6d6eb5ab";
+  $database = "heroku_04f0f2cc6d24dc9";
 
 ?>
