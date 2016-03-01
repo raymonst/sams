e127f675ff8e46690bc54d87f5edf0dba479d53c
raymonst <rsutedjothe@rsutedjothe-ltm.internal.salesforce.com>
2016-02-29 22:05:08 -0800

    changed title for view

--- view.php
+++ view.php
@@ -29,7 +29,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EAT UX</title>
+    <title>Springfield Academy - Applicants List</title>
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link href="css/eat-ux.css" rel="stylesheet">
